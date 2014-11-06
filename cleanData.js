eada6da9ff15b17e44cd265b1b969296b1f65dcc
Mike MacCana <mike.maccana@gmail.com>
2014-11-06 14:02:53 +0000

    Use official list of ISO-3166 country codes

--- cleanData.js
+++ cleanData.js
@@ -12,10 +12,15 @@ var log = console.log.bind(console),
 		console.log(JSON.stringify(object, null, 2))
 	}
 
+
 //We manually converted the file to utf8 from iso8895-1
 var fileContents = fs.readFileSync('GDP.csv');
 
-var INVALID_COUNTRY_CODES = ['WLD','LIC','MIC','LMC','UMC','LMY','EAP','ECA','LAC','MNA','SSA','HIC','EMU']
+// https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes
+var validCountryCodes = jsonFile.readFileSync('validCountryCodes.json');
+
+
+var VALID_COUNTRY_CODES = validCountryCodes.map( function(datum){return datum['alpha-3'] })
 
 csvParse(fileContents, {}, function(err, output){
 
@@ -33,18 +38,18 @@ csvParse(fileContents, {}, function(err, output){
 		var countryCode = entry[0]
 		var country = entry[3].strip(' ')
 		var gdp = entry[4].strip(' ').replace(/,/g, '')
-		var validEntry = countryCode.length === 3 && ( ! INVALID_COUNTRY_CODES.contains(countryCode) ) && ( gdp !== '..' )
+		var validEntry = countryCode.length === 3 && ( VALID_COUNTRY_CODES.contains(countryCode) ) && ( gdp !== '..' )
 		if ( validEntry ) {
 			cleaned.push({
 				country: country,
 				gdp: parseInt(gdp)
 			})
 		} else {
-			log('WARNING: could not parse "', country, '" with GDP', gdp)
+			log('WARNING: could not parse "'+country+'" with GDP', gdp)
 		}
 	})
 
-	jsonFile.writeFile('data/gdpPerCountry.json', cleaned, function(err) {
+	jsonFile.writeFile('json/gdpPerCountry.json', cleaned, function(err) {
 		if (err) {
 			return log(err)
 		}
